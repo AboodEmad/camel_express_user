472ce42ad833577f875e72f7262fd05f84b2ae45
BahaaAlnabeeh <alnbyhbha@gmail.com>
2022-10-03 20:56:10 +0300

    UI

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,6 +1,6 @@
 import 'package:camel_express_user/helpers/app_colors.dart';
-import 'package:camel_express_user/helpers/text_style.dart';
 import 'package:camel_express_user/widget/appbar/custom_appbar.dart';
+import 'package:camel_express_user/widget/elevated_button_with_icon.dart';
 import 'package:camel_express_user/widget/profile/profile_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -191,55 +191,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ElevatedButtonWithIcon extends StatelessWidget {
-  const ElevatedButtonWithIcon({
-    Key? key,
-    required this.title,
-    required this.icon,
-    this.heightButton = 10,
-    this.backgroundColor = AppColors.white,
-    required this.onPressed,
-    this.padding = 17,
-  }) : super(key: key);
-  final String title;
-  final Widget icon;
-  final double padding;
-  final double heightButton;
-  final Color backgroundColor;
-  final void Function() onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: onPressed,
-      style: ButtonStyle(
-        alignment: AlignmentDirectional.centerStart,
-        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        elevation: MaterialStateProperty.all(0),
-        minimumSize:
-            MaterialStateProperty.all(Size(double.infinity.w, heightButton.h)),
-        shape: MaterialStateProperty.all(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.r),
-          ),
-        ),
-        padding: MaterialStateProperty.all(EdgeInsets.zero),
-        backgroundColor: MaterialStateProperty.all(backgroundColor),
-      ),
-      icon: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: padding.w,
-        ),
-        child: icon,
-      ),
-      label: Text(
-        title,
-        style: AppTextStyle.listTileTitle,
       ),
     );
   }
